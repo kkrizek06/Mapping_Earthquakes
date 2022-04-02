a4e5d98b0c0d1eae688d62178a609d1fbc0da56b
kkrizek06 <kkrizek06@gmail.com>
2022-04-02 11:13:11 -0400

    message

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,15 +2,32 @@
 console.log("working");
 
 // Create a map object
-let map = L.map("map").setView([40.7, -94.5],4);
+var map = L.map("map").setView([34.0522, -118.2437],14);
+
+//var marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//add circle
+var cicleMarker = L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color:'black',
+    fillColor: 'yellow'
+}).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+/*var streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
+*/
+// dark layer
+var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "dark-v10",
+  accessToken: API_KEY
+});
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+dark.addTo(map);
